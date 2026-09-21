9a72485fbff9c6862c227bea61ffe1dc1729f744
codebykenneth <amarilajhonkenneth@gmail.com>
2026-09-21 12:29:34 +0800

    adding new repository name Lab Activity

---
+++ Vehicle.java
@@ -0,0 +1,18 @@
+public class Vehicle {
+
+    String brand;
+    String model;
+    int year;
+
+    void displayInfo() {
+        System.out.println("Brand: " + brand + ", Model: " + model + ", Year: " + year);
+    }
+
+    int calculateAge() {
+        return 2026 - year;
+    }
+
+    boolean isVintage() {
+        return calculateAge() > 25;
+    }
+}
