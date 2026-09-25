4cdb47f5745b5139aaa3f5d540a485fcdc68f4ca
Jhon Kenieth Yote amarila <jhonkennethamarila6@gmail.com>
2026-09-25 17:17:27 +0800

    update vehicle

--- Lab Activity 2/Vehicle.java
+++ Lab Activity 2/Vehicle.java
@@ -4,12 +4,6 @@ public class Vehicle {
     String model;
     int year;
 
-    Vehicle(String brand, String model, int year) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-    }
-
     void displayInfo() {
         System.out.println("Brand: " + brand + ", Model: " + model + ", Year: " + year);
     }
